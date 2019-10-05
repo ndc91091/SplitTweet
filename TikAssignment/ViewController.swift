6e6b3eedd0c9ff7d56677f4d1c88a1b970724a9e
cuong <cuongnt04230@gmail.com>
2019-10-05 22:25:55 +0700

    Initial project

---
+++ TikAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TikAssignment
+//
+//  Created by Cuong Nguyen on 10/5/19.
+//  Copyright © 2019 Cuong Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
